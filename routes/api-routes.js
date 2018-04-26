1be3fd42d2be5cab320edf2d4e99706c0364db59
garret <garretgolledge@gmail.com>
2018-04-26 12:48:21 -0700

    changes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -14,39 +14,39 @@ module.exports = function(app) {
 
   // GET route for getting all of the posts
   app.get("/api/posts", function(req, res) {
-    // var query = {};
-    // if (req.query.author_id) {
-    //   query.AuthorId = req.query.author_id;
-    // }
-    // // 1. Add a join here to include all of the Authors to these posts
-    // db.Post.findAll({
-    //   include: [db.Author],
-    //   where: query
-    // }).then(function(dbPost) {
-    //   res.json(dbPost);
-    // });
+    var query = {};
+    if (req.query.author_id) {
+      query.AuthorId = req.query.author_id;
+    }
+    // 1. Add a join here to include all of the Authors to these posts
+    db.Post.findAll({
+      include: [db.Author],
+      where: query
+    }).then(function(dbPost) {
+       res.json(dbPost);
+     });
   });
 
   // Get route for retrieving a single post
   app.get("/api/posts/:id", function(req, res) {
-    // 2. Add a join here to include the Author who wrote the Post
-    // db.Post.findOne({
-    //   include: [db.Author],
-    //   where: {
-    //     id: req.params.id
-    //   }
-    // }).then(function(dbPost) {
-    //   console.log(dbPost);
-    //   res.json(dbPost);
-    // });
+    //2. Add a join here to include the Author who wrote the Post
+    db.Post.findOne({
+      include: [db.Author],
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbPost) {
+      console.log(dbPost);
+      res.json(dbPost);
+    });
   });
 
   // POST route for saving a new post
   app.post("/api/posts", function(req, res) {
-    // db.Post.create(req.body).then(function(dbPost) {
-    //   res.json(dbPost);
-    // });
-  });
+  //   // db.Post.create(req.body).then(function(dbPost) {
+  //   //   res.json(dbPost);
+  //   // });
+  // });
 
   // DELETE route for deleting posts
   app.delete("/api/posts/:id", function(req, res) {
